c426db06c411f7950b764465537b0d17cf1bf9bb
rlackdgml <dhdlqkqh2003@naver.com>
2020-10-19 16:52:00 +0900

    db

---
+++ 오라클/USER1(2020-10-19 게시판).sql
@@ -0,0 +1,16 @@
+-- BBS 프로젝트
+
+DROP TABLE tbl_bbs;
+CREATE TABLE tbl_bbs (
+    b_seq	NUMBER		PRIMARY KEY,
+    b_date	VARCHAR2(10)	NOT NULL,	
+    b_time	VARCHAR2(10)	NOT NULL,	
+    b_writer	NVARCHAR2(30)	NOT NULL,
+    b_subject	NVARCHAR2(125)	NOT NULL,	
+    b_content	NVARCHAR2(2000)	NOT NULL,	
+    b_count	NUMBER,		
+    b_file	NVARCHAR2(125)		
+
+);
+CREATE SEQUENCE SEQ_BBS
+START WITH 1 INCREMENT BY 1;
