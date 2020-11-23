e0a8278ecd1b839fb1866f570a4c796631037d68
rlackdgml <dhdlqkqh2003@naver.com>
2020-11-23 13:39:48 +0900

    mysql

---
+++ MySQL/MYSQL(2020.11.23 spring boot 4).sql
@@ -0,0 +1,21 @@
+USE mysql;
+SHOW TABLES;
+show databases;
+CREATE DATABASE mybbs;
+SHOW DATABASES;
+USE mybbs;
+SHOW TABLES;
+DESC tbl_bbs;
+INSERT INTO tbl_bbs(b_writer,b_date_time,b_content,b_count)
+values('홍길동','2020-11-18','게시판 만들기',0);
+
+
+SELECT * FROM tbl_bbs;
+
+USE mydb;
+drop table tbl_book;
+
+show tables;
+
+
+
